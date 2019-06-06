00483a4ce9778513dec88bb3ce82d4966152749b
santhoshr1699 <50899033+santhoshr1699@users.noreply.github.com>
2019-06-06 23:48:43 +0530

    Create print_if_it_is_a_holiday.py

---
+++ print_if_it_is_a_holiday.py
@@ -0,0 +1,8 @@
+n=input("")   
+i=('Monday','Tuesday','Wednesday','Thursday','Friday','Saturday','Sunday')
+if(n=='Saturday' or n=='Sunday'):
+  print("yes")
+elif(n=='Monday' or n=='Tuesday' or n=='Wednesday' or n=='Thursday' or 'Friday'):
+  print("no")
+else:
+  print("invalid")
